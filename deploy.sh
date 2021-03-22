de397d70bf42eb67b8d0714fb8b9dc7d0280ec7d
Luis Sanchez <luissanchez_1992@hotmail.com>
2021-03-22 22:46:48 +0100

    commit-deploy

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 source name.sh
 nameApp
 git commit -am "commit-deploy"
-ssh MU-NODE-MICRO "cd microservice;git pull;pm2 restart email;pm2 log"
+ssh MU-NODE-MICRO "cd microservice;git pull;pm2 restart mail;pm2 log"
