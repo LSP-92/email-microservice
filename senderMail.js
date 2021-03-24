ca422b972be8ba887235ecc2914c88f7cb99c3fd
Luis Sanchez <luissanchez_1992@hotmail.com>
2021-03-24 01:15:58 +0100

    commit-deploy

--- senderMail.js
+++ senderMail.js
@@ -9,7 +9,7 @@ const createMessage = (to, idTemplate, dynamic) => {
     subject:process.env.SUBJECT,
     from: process.env.EMAIL,
     templateId: idTemplate,
-    dynamicTemplateData: dynamic,
+    dynamicTemplateData: {...dynamic},
   };
 };
 
